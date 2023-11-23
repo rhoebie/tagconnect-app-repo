93e63cefe77dbfa84bbea2183ed2eeba8b0364d0
Rhoebie <rmviruse@gmail.com>
2023-11-23 21:09:11 +0800

    Updateeee

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:taguigconnect/main.dart';
+import 'package:TagConnect/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
