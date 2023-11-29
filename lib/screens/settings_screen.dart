29d92c3a4b0b757ef5b7b9ddbeb1483b3133f209
Rhoebie <rmviruse@gmail.com>
2023-11-29 22:01:43 +0800

    Updateeee

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,5 +1,4 @@
 import 'package:TagConnect/constants/provider_constant.dart';
-import 'package:TagConnect/constants/theme_constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
