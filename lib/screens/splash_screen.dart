f4850acd05c998885f2bd467c0d40ea381c7f530
Rhoebie <rmviruse@gmail.com>
2023-11-30 16:05:51 +0800

    update auto login

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,10 +1,20 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:TagConnect/animations/fade_animation.dart';
+import 'package:TagConnect/configs/network_config.dart';
 import 'package:TagConnect/constants/color_constant.dart';
+import 'package:TagConnect/constants/provider_constant.dart';
+import 'package:TagConnect/models/credential_model.dart';
+import 'package:TagConnect/screens/home_screen.dart';
 import 'package:TagConnect/screens/login_screen.dart';
 import 'package:TagConnect/screens/splash-one_screen.dart';
+import 'package:TagConnect/services/user_service.dart';
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -16,6 +26,81 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+  late AutoLoginNotifier autoLoginNotifier;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    autoLoginNotifier = Provider.of<AutoLoginNotifier>(context, listen: false);
+  }
+
+  Future<void> loadSavedCredentials() async {
+    String email = '';
+    String pass = '';
+    try {
+      final directory = await getApplicationDocumentsDirectory();
+      final file = File('${directory.path}/credentials.txt');
+
+      if (await file.exists()) {
+        // Load JSON from the text file
+        final jsonCredentials = await file.readAsString();
+
+        // Parse JSON into a CredentialModel instance
+        final credentialModel =
+            CredentialModel.fromJson(json.decode(jsonCredentials));
+
+        // Set email and password from CredentialModel
+        email = credentialModel.email ?? '';
+        pass = credentialModel.password ?? '';
+
+        if (autoLoginNotifier.isAutoLogin) {
+          await loginUser(email: email, password: pass);
+        } else {
+          Navigator.of(context)
+              .pushReplacement(FadeAnimation(const LoginScreen()));
+        }
+      } else {
+        print('Credentials file does not exist.');
+      }
+    } catch (e) {
+      print('Error loading credentials: $e');
+    }
+  }
+
+  Future<void> loginUser(
+      {required String email, required String password}) async {
+    try {
+      bool isConnnected = await NetworkService.isConnected();
+      if (isConnnected) {
+        final authService = UserService();
+        final token = await authService.login(email, password);
+
+        if (token != null) {
+          Navigator.of(context)
+              .pushReplacement(FadeAnimation(const HomeScreen()));
+          print('User Token: $token');
+        } else {
+          print('Error');
+        }
+      } else {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) => AlertDialog(
+            title: const Text('No Internet'),
+            content: const Text('Check your internet connection in settings'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () => Navigator.pop(context, 'OK'),
+                child: const Text('OK'),
+              ),
+            ],
+          ),
+        );
+      }
+    } catch (e) {
+      print('Error $e');
+    }
+  }
 
   @override
   void initState() {
@@ -29,6 +114,7 @@ class _SplashScreenState extends State<SplashScreen> {
     try {
       bool isFirstTimeOpen = prefs.getBool('firstOpen') ?? false;
       if (!isFirstTimeOpen) {
+        print('first time open');
         Future.delayed(
           const Duration(seconds: 2),
           () {
@@ -37,13 +123,8 @@ class _SplashScreenState extends State<SplashScreen> {
           },
         );
       } else {
-        Future.delayed(
-          const Duration(seconds: 2),
-          () {
-            Navigator.of(context)
-                .pushReplacement(FadeAnimation(const LoginScreen()));
-          },
-        );
+        loadSavedCredentials();
+        print('not first time open');
       }
     } catch (e) {
       print('Error: $e');
